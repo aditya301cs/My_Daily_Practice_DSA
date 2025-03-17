119a6a3ef4f1bd99045589adc196871228476361
Aditya Kumar <121255083+aditya301cs@users.noreply.github.com>
2025-03-17 23:18:39 +0530

    Time: 0 ms (100%), Space: 19.5 MB (62.35%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -11,30 +11,20 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        // Create a dummy node to act as the starting point of the merged list
-        ListNode dummy(0);
-        ListNode* tail = &dummy;
-        
-        // Traverse both lists
-        while (list1 != nullptr && list2 != nullptr) {
-            if (list1->val < list2->val) {
-                tail->next = list1; // Link the smaller node to the merged list
-                list1 = list1->next; // Move to the next node in list1
-            } else {
-                tail->next = list2; // Link the smaller node to the merged list
-                list2 = list2->next; // Move to the next node in list2
-            }
-            tail = tail->next; // Move the tail pointer
+        if(list1 == NULL){
+            return list2;
         }
-        
-        // Link the remaining nodes from either list1 or list2
-        if (list1 != nullptr) {
-            tail->next = list1;
-        } else {
-            tail->next = list2;
+        if(list2 == NULL){
+            return list1;
         }
-        
-        // Return the head of the merged list
-        return dummy.next;
+        ListNode* result;
+        if(list1->val < list2->val){
+            result = list1;
+            result->next = mergeTwoLists(list1->next,list2);
+        }else{
+            result = list2;
+            result->next = mergeTwoLists(list1,list2->next); 
+        }
+        return result;
     }
 };
