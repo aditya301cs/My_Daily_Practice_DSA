fffc03730dc9984dab3c1c330dea9d85d7e76902
Aditya Kumar <121255083+aditya301cs@users.noreply.github.com>
2025-03-23 23:54:55 +0530

    Time: 11 ms (92.38%), Space: 24.9 MB (85%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.cpp
+++ 0049-group-anagrams/0049-group-anagrams.cpp
@@ -1,38 +1,15 @@
 class Solution {
 public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
-        unordered_map<string, vector<string>>
-            mp; // HashMap to store groups of anagrams
-
-        // Iterate over each word in the input array
-        for (string& s : strs) {
-            vector<int> count(26, 0); // Frequency array to store letter counts
-
-            // Count occurrences of each character in the string
-            for (char ch : s) {
-                count[ch - 'a']++; // Increment the count for the respective
-                                   // character
-            }
-
-            // Generate a unique key from the frequency array
-            string key = "";
-            for (int freq : count) {
-                key += to_string(freq) + "#"; //Creating a unique hashable string by Concatenating frequency values with a "#" separator
-            }
-
-            // Store the string in the corresponding anagram group
-            mp[key].push_back(s);
+        unordered_map<string, vector<string>> map;
+        for (string s : strs) {
+            string sorted = s;
+            sort(sorted.begin(), sorted.end());
+            map[sorted].push_back(s);
         }
-
-        // Convert the unordered_map values into a vector of anagram groups
         vector<vector<string>> result;
-        for (auto& it : mp) {
-            //result.push_back(it.second);     //it.second (the vector) into result, which can be slow for large input sizes.
-            // std::move(it.second) is used to transfer ownership of the vector inside the unordered_map to result without making a deep copy.
-            result.push_back(move(it.second)); //This moves it.second, leaving it empty in mp after transfer or Move to optimize performance
-        }
-
+        for (auto& pair : map)
+            result.push_back(pair.second);
         return result;
     }
-}
-;
+};
