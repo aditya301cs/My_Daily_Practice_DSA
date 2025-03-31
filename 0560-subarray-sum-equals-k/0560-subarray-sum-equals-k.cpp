069f94d7e13f88055824bd9a29b042dcb730486b
Aditya Kumar <121255083+aditya301cs@users.noreply.github.com>
2025-03-31 17:22:47 +0530

    Time: 45 ms (62.19%), Space: 45.5 MB (55.86%) - LeetHub

--- 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
+++ 0560-subarray-sum-equals-k/0560-subarray-sum-equals-k.cpp
@@ -1,19 +1,19 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        unordered_map<int, int> presum;
-        presum[0] = 1; // Assume prefix sum 0 at index -1
+        unordered_map<int, int> prefixMap;
+        prefixMap[0] = 1; // Assume prefix sum 0 at index -1
         int sum = 0, count = 0;
 
         for (int num : nums) {
             sum += num;
 
             // Check if (sum - k) exists in the map
-            if (presum.find(sum - k) != presum.end()) 
-                count += presum[sum - k];
+            if (prefixMap.find(sum - k) != prefixMap.end()) 
+                count += prefixMap[sum - k];
 
             // Store/update the frequency of the current prefix sum
-            presum[sum]++;
+            prefixMap[sum]++;
         }
         
         return count;
