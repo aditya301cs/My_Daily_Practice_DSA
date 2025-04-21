cd2ff7d383b9457bc8d6143fc1b8a4fc45e6bfd6
Aditya Kumar <121255083+aditya301cs@users.noreply.github.com>
2025-04-21 10:55:50 +0530

    Time: 6 ms (35.06%), Space: 127.5 MB (21.26%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -1,34 +1,26 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+#include <stack>
+
 class Solution {
 public:
     bool isPalindrome(ListNode* head) {
-        vector<int> arr;
-
-        // Step 1: Store the linked list values in an array
+        stack<int> st;
         ListNode* temp = head;
-        while (temp != NULL) {
-            arr.push_back(temp->val);
+
+        // Step 1: Push all values to stack
+        while (temp) {
+            st.push(temp->val);
             temp = temp->next;
         }
 
-        // Step 2: Use two-pointer approach to check for palindrome
-        int left = 0, right = arr.size() - 1;
-        while (left < right) {
-            if (arr[left] != arr[right])
-                return false;
-            left++;
-            right--;
+        // Step 2: Compare while popping
+        temp = head;
+        while (temp) {
+            int top = st.top();
+            st.pop();
+            if (top != temp->val) return false;
+            temp = temp->next;
         }
 
-        return true; // If all elements match, it's a palindrome
+        return true;
     }
 };
