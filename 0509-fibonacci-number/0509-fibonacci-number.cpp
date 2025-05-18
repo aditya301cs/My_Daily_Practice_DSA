f08de967a9b0e7258ef2f7260e31e4e10382f94d
Aditya Kumar <121255083+aditya301cs@users.noreply.github.com>
2025-05-19 00:25:42 +0530

    Time: 143 ms (21.52%), Space: 7.9 MB (37.65%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int t[31];
+
+    int solve(int n) {
+        if (n <= 1)
+            return n;
+
+        if (t[n] != -1)
+            return t[n];
+
+        return t[n] = fib(n - 1) + fib(n - 2);
+    }
+
+    int fib(int n) {
+        memset(t, -1, sizeof(t));
+
+        return solve(n);
+    }
+};
