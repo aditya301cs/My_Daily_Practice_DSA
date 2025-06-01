a16c95573cd53a6691f2483c1d24f71026792cb5
Aditya Kumar <121255083+aditya301cs@users.noreply.github.com>
2025-06-01 13:54:29 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -1,11 +1,23 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int minPrice = INT_MAX, maxProfit = 0;
-        for (int price : prices) {
-            minPrice = min(minPrice, price);
-            maxProfit = max(maxProfit, price - minPrice);
+        return solve(0,1,1,prices);
+    }
+
+    int solve(int idx, int canBuy, int transCount, vector<int>& prices){
+        if(idx >= prices.size()) return 0;
+        if(transCount == 0) return 0;
+
+
+        if(canBuy == 1){
+            int idle = solve(idx + 1, 1, transCount, prices);
+            int buy = -prices[idx] + solve(idx + 1, 0, transCount, prices);
+            return max(idle, buy);
         }
-        return maxProfit;
+        else{
+            int idle = solve(idx + 1, 0, transCount, prices);
+            int sell = prices[idx] + solve(idx + 1, 1, transCount - 1, prices);
+            return max(idle, sell);
+        }
     }
 };
