26af6d29926af496e9ee373c6f22afffc698b443
Aditya Kumar <121255083+aditya301cs@users.noreply.github.com>
2025-08-23 23:29:32 +0530

    Time: 0 ms (100%), Space: 14.6 MB (31.4%) - LeetHub

---
+++ 1094-car-pooling/1094-car-pooling.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool carPooling(vector<vector<int>>& trips, int capacity) {
+        // Because from and to is between 0 and 1000. Idea is to store counts in an array of size 1001.
+        vector<int> lengthOfTrip = vector<int>(1001);
+        for (auto trip : trips){
+            lengthOfTrip[trip[1]] += trip[0]; // Increment when passenger is on board
+            lengthOfTrip[trip[2]] -= trip[0]; // decrement when they depart
+        }
+        // Count total passenger for each bus top
+        int carLoad = 0;
+        // we have the count array, we perform a line sweep from 0 to 1000 and track its total
+        for (int i = 0; i < 1001 && carLoad <= capacity; i++){
+            carLoad += lengthOfTrip[i];
+            // Reject when the car is overloaded somewhere
+            if(carLoad > capacity) return false;
+        }
+
+        return true; // Accept only if all trip is safe
+    }
+};
