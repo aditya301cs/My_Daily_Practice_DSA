b0a9027e8afe40ef9391e087248f5ecc6de90c16
Aditya Kumar <121255083+aditya301cs@users.noreply.github.com>
2025-09-30 17:07:26 +0530

    Time: 109 ms (5.02%), Space: 79.1 MB (13.8%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -2,23 +2,20 @@ class Solution {
 public:
     int m,n;
     int t[501][501];
-    int solve(string s1, string s2, int i, int j){
-        if(i == m){
-            return n - j;
-        } 
-        else if(j == n){
-            return m - i;
-        } 
-        if(t[i][j] != -1) return t[i][j];
-        
-        if(s1[i] == s2[j]){
-            return t[i][j] = solve(s1, s2, i+1, j+1);
-        }else{
-            int insertC = 1 + solve(s1, s2, i, j+1);
-            int deleteC = 1 + solve(s1, s2, i+1, j);
-            int replaceC = 1 + solve(s1, s2, i+1, j+1);
+    int solve(string s1, string s2, int m, int n){
+        if(m == 0 || n == 0){
+            return m+n;
+        }
+        if(t[m][n] != -1) return t[m][n];
+
+        if(s1[m-1] == s2[n-1])
+            return t[m][n] = solve(s1, s2, m-1, n-1);
+        else {
+            int insertC  = 1 + solve(s1, s2, m, n-1);
+            int deleteC  = 1 + solve(s1, s2, m-1, n);
+            int replaceC = 1 + solve(s1, s2, m-1, n-1);
             
-            return t[i][j] = min({insertC, deleteC, replaceC});
+            return t[m][n] = min({insertC, deleteC, replaceC});
         }
         return -1;
     }
@@ -26,6 +23,6 @@ public:
         m = word1.size();
         n = word2.size();
         memset(t, -1, sizeof(t));
-        return solve(word1, word2, 0, 0);
+        return solve(word1, word2, m, n);
     }
 };
