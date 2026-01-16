47054fdbdf21b04e4093a82ca72758c68e9af7e3
Aditya Kumar <121255083+aditya301cs@users.noreply.github.com>
2026-01-17 00:44:45 +0530

    Time: 0 ms (100%), Space: 15 MB (9.58%) - LeetHub

--- 0086-partition-list/0086-partition-list.cpp
+++ 0086-partition-list/0086-partition-list.cpp
@@ -8,28 +8,30 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+//Time : O(n), we visit each node only once
 class Solution {
 public:
     ListNode* partition(ListNode* head, int x) {
-        ListNode* temp1 = new ListNode(0);  
-        ListNode* temp2 = new ListNode(0);  
-        ListNode* t1 = temp1;  
-        ListNode* t2 = temp2;  
-
-        while (head) {
-            if (head->val < x) {
-                t1->next = head;  
-                t1 = t1->next;
+        ListNode* small  = new ListNode(0);
+        ListNode* large  = new ListNode(0);
+        
+        ListNode* smallP = small;
+        ListNode* largeP = large;
+        
+        while(head) {
+            if(head->val < x) {
+                smallP->next = head;
+                smallP = smallP->next;
             } else {
-                t2->next = head;  
-                t2 = t2->next;
+                largeP->next = head;
+                largeP = largeP->next;
             }
+            
             head = head->next;
         }
-
-        t2->next = nullptr;   
-        t1->next = temp2->next;  
-
-        return temp1->next;  
+        
+        largeP->next = NULL;
+        smallP->next = large->next;
+        return small->next;
     }
 };
