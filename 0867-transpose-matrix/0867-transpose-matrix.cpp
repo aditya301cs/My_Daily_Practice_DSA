47bcda8644bf42e913ecc75ac0b2f4414673eeb8
Aditya Kumar <121255083+aditya301cs@users.noreply.github.com>
2026-04-12 20:04:16 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0867-transpose-matrix/0867-transpose-matrix.cpp
+++ 0867-transpose-matrix/0867-transpose-matrix.cpp
@@ -1,18 +1,14 @@
 class Solution {
 public:
     vector<vector<int>> transpose(vector<vector<int>>& matrix) {
-        int m = matrix.size();
-        int n = matrix[0].size();
-
-        // To create a mxn normal matrix 
-      //vector<vector<int>> matrix(rows, vector<int>(cols));
-
-        vector<vector<int>> result(n, vector<int>(m));  //n*m for transpose matrix
-        for(int i = 0; i < m; i++){
-            for(int j = 0; j < n; j++){
-                result[j][i] = matrix[i][j];
-            }
+     int m = matrix.size();
+     int n = matrix[0].size();
+     vector<vector<int>>ans(n, vector<int>(m));
+     for(int i = 0; i < m; i++){
+        for(int j = 0; j<n; j++){
+            ans[j][i] = matrix[i][j];
         }
-        return result;
+     }
+        return ans;
     }
 };
