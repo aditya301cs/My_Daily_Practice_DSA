d62524a914965e132562e810945ebe1bb4a967e7
Aditya Kumar <121255083+aditya301cs@users.noreply.github.com>
2026-04-26 22:40:53 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,19 +1,20 @@
 class Solution {
 public:
     vector<vector<int>> result;
-    void solve(vector<int>& nums, int idx, vector<int>& temp) {
-        if (idx >= nums.size()) {
+    void solve(int i, vector<int>& nums, vector<int>& temp) {
+        if (i >= nums.size()) {
             result.push_back(temp);
             return;
         }
-        temp.push_back(nums[idx]);
-        solve(nums, idx + 1, temp);
+
+        temp.push_back(nums[i]);
+        solve(i + 1, nums, temp);
         temp.pop_back();
-        solve(nums, idx + 1, temp);
+        solve(i + 1, nums, temp);
     }
     vector<vector<int>> subsets(vector<int>& nums) {
         vector<int> temp;
-        solve(nums, 0, temp);
+        solve(0, nums, temp);
         return result;
     }
 };
