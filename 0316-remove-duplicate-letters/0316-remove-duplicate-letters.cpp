ec704312d941bd5f6d6a10427a4c017a79b6184f
Aditya Kumar <121255083+aditya301cs@users.noreply.github.com>
2026-05-02 23:45:27 +0530

    Time: 0 ms (100%), Space: 9.2 MB (71.66%) - LeetHub

---
+++ 0316-remove-duplicate-letters/0316-remove-duplicate-letters.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    string removeDuplicateLetters(string s) {
+        vector<int> freq(26, 0);
+        vector<bool> visited(26, false);
+
+        for (char c : s) {
+            freq[c - 'a']++;
+        }
+
+        string result = "";
+
+        for (char c : s) {
+            freq[c - 'a']--;
+
+            if (visited[c - 'a']) continue;
+
+            while (!result.empty() &&
+                   result.back() > c &&
+                   freq[result.back() - 'a'] > 0) {
+                visited[result.back() - 'a'] = false;
+                result.pop_back();
+            }
+
+            result.push_back(c);
+            visited[c - 'a'] = true;
+        }
+
+        return result;
+    }
+};
