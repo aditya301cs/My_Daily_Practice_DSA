265f9b18074f21a6ef24608cccb94eec135bbd25
Aditya Kumar <121255083+aditya301cs@users.noreply.github.com>
2026-05-09 15:44:08 +0530

    Time: 407 ms (23.88%), Space: 89.9 MB (5.04%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,26 +1,22 @@
 class Solution {
 public:
-    int solve(int i, int currentSum, vector<int>& nums, int target, int totalSum, vector<vector<int>>&dp){
+    int solve(int i, int currentSum, vector<int>& nums, int target, unordered_map<string, int>&mp){
         if(i == nums.size()){
             if(currentSum == target) return true;
             else return false;
         }
-        if(dp[i][currentSum + totalSum] != -1){
-            return dp[i][currentSum + totalSum];
-        }
-        int plus = solve(i+1, currentSum + nums[i], nums, target, totalSum, dp);
-        int minus = solve(i+1, currentSum - nums[i], nums, target, totalSum, dp);
+        string key = to_string(i) + "_" + to_string(currentSum);
 
-        return dp[i][currentSum + totalSum] = plus + minus;
+        if(mp.count(key)){ //(mp.find(key) != mp.end())
+            return mp[key];
+        }
+        int plus = solve(i+1, currentSum + nums[i], nums, target, mp);
+        int minus = solve(i+1, currentSum - nums[i], nums, target, mp);
+
+        return mp[key] = plus+minus;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
-        int n = nums.size();
-        int totalSum = 0;
-        for(int num : nums){
-            totalSum += num;
-        } 
-        // range = [-totalSum , totalSum]
-        vector<vector<int>>dp(n+1, vector<int>((2*totalSum )+ 1, -1));
-        return solve(0, 0, nums, target, totalSum, dp);
+        unordered_map<string,  int>mp;
+        return solve(0, 0, nums, target, mp);
     }
 };
