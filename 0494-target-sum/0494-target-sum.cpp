2f3d8dbac50b9b8abb734e777b3ea2500a466aab
Aditya Kumar <121255083+aditya301cs@users.noreply.github.com>
2026-05-09 15:36:26 +0530

    Time: 16 ms (46.32%), Space: 26.3 MB (20.31%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,17 +1,26 @@
 class Solution {
 public:
-    int solve(int i, int currentSum, vector<int>& nums, int target){
+    int solve(int i, int currentSum, vector<int>& nums, int target, int totalSum, vector<vector<int>>&dp){
         if(i == nums.size()){
             if(currentSum == target) return true;
             else return false;
         }
+        if(dp[i][currentSum + totalSum] != -1){
+            return dp[i][currentSum + totalSum];
+        }
+        int plus = solve(i+1, currentSum + nums[i], nums, target, totalSum, dp);
+        int minus = solve(i+1, currentSum - nums[i], nums, target, totalSum, dp);
 
-        int plus = solve(i+1, currentSum + nums[i], nums, target);
-        int minus = solve(i+1, currentSum - nums[i], nums, target);
-
-        return plus+minus;
+        return dp[i][currentSum + totalSum] = plus + minus;
     }
     int findTargetSumWays(vector<int>& nums, int target) {
-        return solve(0, 0, nums, target);
+        int n = nums.size();
+        int totalSum = 0;
+        for(int num : nums){
+            totalSum += num;
+        } 
+        // range = [-totalSum , totalSum]
+        vector<vector<int>>dp(n+1, vector<int>((2*totalSum )+ 1, -1));
+        return solve(0, 0, nums, target, totalSum, dp);
     }
 };
