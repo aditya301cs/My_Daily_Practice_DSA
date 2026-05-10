66760becb9c5d7052a8e0f089515acaf603442f1
Aditya Kumar <121255083+aditya301cs@users.noreply.github.com>
2026-05-10 15:51:36 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -2,25 +2,25 @@ class Solution {
 public:
     int dp[13][10001];
 
-    int solve(vector<int>& coins, int n, int sum) {
+    int solve(vector<int>& coins, int n, int amount) {
 
-        if(sum == 0) return 0;
+        if(amount == 0) return 0;
 
         if(n == 0) return INT_MAX - 1;
 
-        if(dp[n][sum] != -1) {
-            return dp[n][sum];
+        if(dp[n][amount] != -1) {
+            return dp[n][amount];
         }
 
         int take = INT_MAX - 1;
 
-        if(coins[n-1] <= sum) {
-            take = 1 + solve(coins, n, sum - coins[n-1]);
+        if(coins[n-1] <= amount) {
+            take = 1 + solve(coins, n, amount - coins[n-1]);
         }
 
-        int notTake = solve(coins, n-1, sum);
+        int notTake = solve(coins, n-1, amount);
 
-        return dp[n][sum] = min(take, notTake);
+        return dp[n][amount] = min(take, notTake);
     }
 
     int coinChange(vector<int>& coins, int amount) {
