d5c740c02dda4ac4f11f63d6df18a47526c8d81a
Aditya Kumar <121255083+aditya301cs@users.noreply.github.com>
2026-05-11 22:11:29 +0530

    Time: 0 ms (100%), Space: 17.6 MB (53.25%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,21 +1,49 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-        int n = nums.size();
-
-        int first_pos = -1;
-        int second_pos = -1;
-
-        for(int i = 0; i < n; i++) {
-            if(nums[i] == target) {
-
-                if(first_pos == -1)
-                    first_pos = i;
-
-                second_pos = i;
+    int findLeftMost(vector<int>& nums, int target, int n){
+        int l = 0;
+        int r = n-1;
+        int left_most = -1;
+        while(l<=r){
+            int mid = l + (r-l)/2;
+            if(nums[mid]==target){
+                left_most = mid;  //Possibly by answer
+                r = mid - 1;      // look more on left
+            }
+            else if(nums[mid]<target){
+                l = mid + 1;
+            }
+            else{
+                r = mid - 1; 
             }
         }
+        return left_most;
+    }
+    int findRightMost(vector<int>& nums, int target, int n){
+        int l = 0;
+        int r = n-1;
+        int right_most = -1;
+        while(l<=r){
+            int mid = l + (r-l)/2;
+            if(nums[mid]==target){
+                right_most = mid;  //Possibly by answer
+                l = mid + 1;
+            }
+            else if(nums[mid]<target){
+                l = mid + 1; 
+            }
+            else{
+                r = mid - 1; 
+            }
+        }
+        return right_most;
+    }
 
-        return {first_pos, second_pos};
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int n = nums.size();
+        int left_most = findLeftMost(nums, target, n);
+        int right_most = findRightMost(nums, target, n);
+
+        return {left_most,right_most};
     }
 };
