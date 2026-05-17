b2d1d35715c5d5008eb44907a4b56de0d9c6aa45
Aditya Kumar <121255083+aditya301cs@users.noreply.github.com>
2026-05-17 15:15:03 +0530

    Time: 1247 ms (5.03%), Space: 26.9 MB (36.55%) - LeetHub

--- 0044-wildcard-matching/0044-wildcard-matching.cpp
+++ 0044-wildcard-matching/0044-wildcard-matching.cpp
@@ -1,21 +1,29 @@
 class Solution {
 public:
+
     int dp[2001][2001];
+
     bool solve(string &s, string &p, int n, int m){
-        if(n < 0 &&  m < 0){
+
+        // both strings exhausted
+        if(n == 0 && m == 0){
             return true;
         }
 
-        if(n >= 0 && m < 0){
+        // pattern exhausted
+        if(m == 0){
             return false;
         }
 
-        if(n < 0 && m >= 0){
-            for(int k = 0; k <= m; k++){
-                if(p[k] != '*'){
+        // string exhausted
+        if(n == 0){
+
+            for(int k = 1; k <= m; k++){
+                if(p[k-1] != '*'){
                     return false;
                 }
             }
+
             return true;
         }
 
@@ -23,23 +31,27 @@ public:
             return dp[n][m];
         }
 
-        // single character match
-        if(s[n] == p[m] || p[m] == '?'){
-            return dp[n][m] = solve(s, p, n-1, m-1);
+        // character match
+        if(s[n-1] == p[m-1] || p[m-1] == '?'){
+            return dp[n][m] =
+                solve(s, p, n-1, m-1);
         }
-        // '*' can:
-        // 1. match one character -> (n-1, m)
-        // 2. match empty string -> (n, m-1)
-        else if(p[m] == '*'){
-            return dp[n][m] = solve(s, p, n-1, m) || solve(s, p, n, m-1);
+
+        // star case
+        else if(p[m-1] == '*'){
+
+            return dp[n][m] =
+                solve(s, p, n-1, m) ||   // take character
+                solve(s, p, n, m-1);     // take empty
         }
-        // mismatch
-        else{
-            return dp[n][m] = false;
-        }
+
+        return dp[n][m] = false;
     }
+
     bool isMatch(string s, string p) {
+
         memset(dp, -1, sizeof(dp));
-        return solve(s, p , s.length() - 1, p.length()-1);
+
+        return solve(s, p, s.length(), p.length());
     }
 };
